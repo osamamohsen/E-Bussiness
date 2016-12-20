65f30dc9a7e77e2d6ac619eb2fefc0a5526ed009
osamamohsen <osamamohsen1994@gmail.com>
2016-12-20 15:50:43 +0200

    add new ifle

--- sites/all/modules/login_js/login_js.js
+++ sites/all/modules/login_js/login_js.js
@@ -3,25 +3,60 @@
  */
 
 jQuery(document).ready(function () {
-    jQuery('#login-js-page').on('submit',function () {
+    // jQuery('#login-js-page').on('submit',function () {
+    //     var that = jQuery(this),
+    //         url = '/drup/drupal-7.53/api/login_page ',//that.attr('action'),
+    //         type = that.attr('method'),
+    //         data = [];
+    //     // alert(type);
+    //     // alert(url);
+    //     var res = [jQuery('#edit-name'),jQuery('#edit-password')];
+    //     for(var i=0;i<res.length;i++){
+    //         var name = res[i].attr('name');
+    //         var value = res[i].val();
+    //         data[name] = value;
+    //     }
+    //     console.log(data);
+    //     jQuery.ajax({
+    //         url : url,
+    //         type : type,
+    //         data : data,
+    //         success: function (response) {
+    //             console.log(response);
+    //             jQuery('#errors').html("Done Data"+data);
+    //         },
+    //         error: function (e) {
+    //             console.log(e);
+    //             jQuery('#errors').html("Error Data");
+    //         }
+    //     });
+    //     return false;
+    // });
+    jQuery('#login-js-page').on('submit',function (e) {
+        // e.preventDefault();
         var that = jQuery(this),
-            url = '/drup/drupal-7.53/?q=course/API/v1/question-data',//that.attr('action'),
+            url = 'api/login_page ',//that.attr('action'),
             type = that.attr('method'),
             data = [];
         // alert(type);
         // alert(url);
+        // that
+        console.log();
         var res = [jQuery('#edit-name'),jQuery('#edit-password')];
         for(var i=0;i<res.length;i++){
             var name = res[i].attr('name');
             var value = res[i].val();
             data[name] = value;
         }
+        console.log(type);
         console.log(data);
+        console.log('http://localhost/drup/drupal-7.53/'+url);
         jQuery.ajax({
-            url : url,
+            url : 'api/login_page',
             type : type,
             data : data,
             success: function (response) {
+                // alert(response);
                 console.log(response);
                 jQuery('#errors').html("Done Data"+data);
             },
@@ -33,3 +68,20 @@ jQuery(document).ready(function () {
         return false;
     });
 });
+
+function ajax_form() {
+    jQuery.ajax({
+        url : 'api/login_page',
+        type : type,
+        data : data,
+        success: function (response) {
+            // alert(response);
+            console.log(response);
+            jQuery('#errors').html("Done Data"+data);
+        },
+        error: function (e) {
+            console.log(e);
+            jQuery('#errors').html("Error Data");
+        }
+    });
+}
